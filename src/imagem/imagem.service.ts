a13491e2c272fdbd4b999b18976ff1331122a080
rjunior2209 <132413307+rjunior2209@users.noreply.github.com>
2024-05-09 21:59:50 -0300

    crud de imagem

--- src/imagem/imagem.service.ts
+++ src/imagem/imagem.service.ts
@@ -12,4 +12,29 @@ export class ImagemService {
     async findAll(): Promise<Imagem[]>{
         return await this.imagemRepository.find()
     }
+
+    async encontraImagemId(idimagem: number): Promise<Imagem>{
+        const imagem = await this.imagemRepository.findOneBy({idimagem});
+        if(!imagem){
+            return null
+        }
+        return imagem;
+    }
+
+    async cadastrarImagem(imagemData: Partial<Imagem>): Promise<Imagem>{
+        return this.imagemRepository.save(imagemData);
+    }
+    async alteraImagem(idimagem: number, imagemData: Partial<Imagem>): Promise<Imagem>{
+        await this.imagemRepository.update(idimagem, imagemData)
+        return this.encontraImagemId(idimagem)
+    }
+     async deletaImagem(idimagem: number): Promise<void>{
+        const imagem = await this.imagemRepository.findOneBy({idimagem});
+        if (!imagem){
+        return null
+        }
+        await this.imagemRepository.delete(idimagem);
+     }
+    
+    
 }
