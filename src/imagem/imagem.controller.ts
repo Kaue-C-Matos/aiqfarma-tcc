eb132a0e6424522fa85d72be80aee9bb35c7f66e
rjunior2209 <132413307+rjunior2209@users.noreply.github.com>
2024-05-09 23:59:06 -0300

    CONTROLLER DE IMAGEM

--- src/imagem/imagem.controller.ts
+++ src/imagem/imagem.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, NotFoundException, Param, Post, Put,Delete, HttpStatus } from "@nestjs/common";
 import { ImagemService } from "./imagem.service";
 import { Imagem } from "./imagem.entity";
+import Response from "superagent/lib/node/response";
+
 
 @Controller("imagem")
 export class ImageController {
@@ -9,5 +11,39 @@ export class ImageController {
     getImagem(): Promise<Imagem[]>{
         return this.imagemService.findAll()
     }
+    
+    @Get('id/:id')
+    async getImagemId(@Param('id') id:number): Promise<Imagem>{
+        const imagem = await this.imagemService.encontraImagemId(id)
+        if (imagem==null){
+            throw new NotFoundException('Imagem não encontrada, favor verifique o ID inserido');
+        }
+        return imagem
+    }
+
+    @Post()
+    postImagem(@Body() imagemData: Partial<Imagem>): Promisse<Imagem>{
+        return this.imagemService.cadastrarImagem(imagemData)
+     }
+
+     @Put(':id')
+     async putImagem(@Param('id') id: number, @Body() imagemData: Partial<Imagem>): Promise<Imagem>{
+        const imagem = await this.imagemService.alteraImagem(id, imagemData)
+        if(imagem==null){
+            throw new NotFoundException('Imagem não encontrada, favor verifique o ID inserido');
+        } 
+        return imagem;
+    }
+    @Delete(':id')
+    async deletaImagem (@Param('id') id: number, @Response() res){
+        const imagem = await this.imagemService.encontraImagemId(id)
+        if(imagem == null){
+            return new NotFoundException('Imagem não encontrada, favor verifique o ID inserido');
+        }else{
+            await this.imagemService.deletaImagem(id)
+            return res.status(HttpStatus.OK).json('Produto deletado')
+        }
+    }
+
     
 }
