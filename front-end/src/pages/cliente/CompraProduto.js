3edf17f10b28b2eb3a3d977332ebbbbea8822f48
rjunior2209 <132413307+rjunior2209@users.noreply.github.com>
2024-06-19 21:52:46 -0300

    adição de valor total e de imput do endereco

--- front-end/src/pages/cliente/CompraProduto.js
+++ front-end/src/pages/cliente/CompraProduto.js
@@ -1,5 +1,5 @@
 import { ArrowLeftOutlined, MinusCircleFilled, PlusCircleFilled } from "@ant-design/icons"
-import { Image, Form, Select, Button } from "antd"
+import { Image, Form, Select, Button, Checkbox, Input } from "antd"
 import Title from "antd/es/typography/Title"
 import axios from "axios"
 import { useCallback, useEffect, useState } from "react"
@@ -7,8 +7,12 @@ import { useNavigate, useParams } from "react-router-dom"
 import { useAuth } from "../../context/AutorizaçãoContext"
 
 function CompraProduto() {
+
+    const [componentDisabled, setComponentDisabled] = useState(true);
+    const [colorInput, setColorInput] = useState("#D9D9D9")
+
     const navigate = useNavigate()
-    const {setEstaAutorizado} = useAuth()
+    const { setEstaAutorizado } = useAuth()
 
     const produtoEscolhido = useParams().id
 
@@ -21,12 +25,12 @@ function CompraProduto() {
 
     const fetchData = useCallback(async () => {
         try {
-          const produtoResponse = await axios.get(`http://localhost:3000/produtos/id/${produtoEscolhido}`)
-          const produtoData = produtoResponse.data
-          setProduto(produtoData)
-          
-          const imagemResponse = await axios.get(`http://localhost:3000/imagem/id/${produtoData.id_imagem}`)
-          setImagem(imagemResponse.data)
+            const produtoResponse = await axios.get(`http://localhost:3000/produtos/id/${produtoEscolhido}`)
+            const produtoData = produtoResponse.data
+            setProduto(produtoData)
+
+            const imagemResponse = await axios.get(`http://localhost:3000/imagem/id/${produtoData.id_imagem}`)
+            setImagem(imagemResponse.data)
         } catch (error) {
             console.error('Erro ao buscar dados:', error);
         }
@@ -35,7 +39,7 @@ function CompraProduto() {
     async function comprar(valores) {
         await axios.post('http://localhost:3000/pedidos', {
             valor_total: (quantidadeTotal * produto.preco),
-            cliente: "Julia Kauani",
+            cliente: "Julia Kawani",
             quantidade: quantidadeTotal,
             id_endereco_cliente: 8,
             id_produto: produtoEscolhido,
@@ -44,7 +48,7 @@ function CompraProduto() {
             retirada: valores.entrega
         })
 
-        await axios.patch(`http://localhost:3000/produtos/quantidade/${produtoEscolhido}`,{
+        await axios.patch(`http://localhost:3000/produtos/quantidade/${produtoEscolhido}`, {
             quantidade: produto.quantidade - quantidadeTotal
         })
 
@@ -52,41 +56,49 @@ function CompraProduto() {
         navigate("/confirmacao")
     }
 
-    function diminuir(){
-        if (quantidadeTotal > 1){
-            setQuantidadeTotal(quantidadeTotal-1)
-        }else{
+    function diminuir() {
+        if (quantidadeTotal > 1) {
+            setQuantidadeTotal(quantidadeTotal - 1)
+        } else {
             setQuantidadeTotal(quantidadeTotal)
         }
     }
 
-    function aumentar(){
-        if (quantidadeTotal < produto.quantidade){
-            setQuantidadeTotal(quantidadeTotal+1)
-        }else{
+    function aumentar() {
+        if (quantidadeTotal < produto.quantidade) {
+            setQuantidadeTotal(quantidadeTotal + 1)
+        } else {
             setQuantidadeTotal(quantidadeTotal)
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         if (quantidadeTotal === 1) {
             setBotaoDiminuir("#9d9d9d")
-        } else if(quantidadeTotal === produto.quantidade){
+        } else if (quantidadeTotal === produto.quantidade) {
             setBoatoAumentar("#9d9d9d")
         } else {
             setBotaoDiminuir("#610E00")
             setBoatoAumentar("#610E00")
         }
-        
-    }, [diminuir])
 
-    useEffect(()=>{
+    }, [quantidadeTotal, produto.quantidade])
+
+    useEffect(() => {
         fetchData()
     }, [fetchData])
 
+    useEffect(() => {
+        if (componentDisabled) {
+            setColorInput("#D9D9D9")
+        } else {
+            setColorInput("#000")
+        }
+    }, [componentDisabled])
+
     return (
         <div>
-            <ArrowLeftOutlined onClick={()=> navigate(-1)} style={{ fontSize: "20px", padding: "5px 8px", margin: "10px 10px", color: "#FFFF", backgroundColor: "#610E00", borderRadius: "5px" }} />
+            <ArrowLeftOutlined onClick={() => navigate(-1)} style={{ fontSize: "20px", padding: "5px 8px", margin: "10px 10px", color: "#FFFF", backgroundColor: "#610E00", borderRadius: "5px" }} />
 
             <Title level={2} style={{ margin: "17px", display: "flex", justifyContent: "center", textAlign: "center", color: "#610E00" }}>
                 Finalize sua Compra!!!
@@ -96,7 +108,7 @@ function CompraProduto() {
                 Produto
             </Title>
 
-            <div style={{ display: "flex", backgroundColor: "#D9D9D9", width: 270, padding: "10px 0px", gap: 20, margin: 4, borderRadius: 5, alignItems: "center",}}>
+            <div style={{ display: "flex", backgroundColor: "#D9D9D9", width: 270, padding: "10px 0px", gap: 20, margin: 4, borderRadius: 5, alignItems: "center", }}>
                 <Image width={220} style={{ margin: "5px" }} src={imagem.imagem} />
                 <b>{produto.nome}</b>
             </div>
@@ -107,36 +119,49 @@ function CompraProduto() {
                 </Title>
 
                 <div style={{ display: "flex", marginTop: 12, gap: 8 }}>
-                    <MinusCircleFilled style={{ fontSize: 25, color: botaoDiminuir }} onClick={() => diminuir()}/>
+                    <MinusCircleFilled style={{ fontSize: 25, color: botaoDiminuir }} onClick={() => diminuir()} />
                     <p>{quantidadeTotal}</p>
                     <PlusCircleFilled style={{ fontSize: 25, color: botaoAumentar }} onClick={() => aumentar()} />
                 </div>
+
             </div>
 
-            <Form onFinish={comprar} style={{margin:"0 10px"}}>
-                    <Form.Item required name="entrega" label={<label style={{ fontSize: 22, color: "#610E00" }}><b>Retirada do Produto</b></label>}>
-                        <Select style={{ width: "180px", border: "2px solid", borderRadius: 8 }}>
-                            <Select.Option value="Entrega a domicilio">Entrega a domicilio</Select.Option>
-                            <Select.Option value="Retirada no local">Retirada no local</Select.Option>
-                        </Select>
-                    </Form.Item>
+            <Form onFinish={comprar} style={{ margin: "0 10px" }}>
+                <Form.Item required name="entrega" label={<label style={{ fontSize: 22, color: "#610E00" }}><b>Retirada do Produto</b></label>}>
+                    <Select style={{ width: "180px", border: "2px solid", borderRadius: 8 }}>
+                        <Select.Option value="Entrega a domicilio">Entrega a domicilio</Select.Option>
+                        <Select.Option value="Retirada no local">Retirada no local</Select.Option>
+                    </Select>
+                </Form.Item>
 
-                    <Form.Item required name="pagamento" label={<label style={{ fontSize: 22, color: "#610E00" }}><b>Forma de Pagamento</b></label>}>
-                        <Select style={{ width: "180px", border: "2px solid", borderRadius: 8 }}>
-                            <Select.Option value="Pix">Pix</Select.Option>
-                            <Select.Option value="Cartao de crédito">Cartão de Credito</Select.Option>
-                            <Select.Option value="Dinheiro">Dinheiro</Select.Option>
-                        </Select>
-                    </Form.Item>
+                <Checkbox style={{ fontSize: 18, color: "#610E00" }}
+                    checked={componentDisabled}
+                    onChange={(e) => setComponentDisabled(e.target.checked)}>
+                    <b>Utilizar endereço padrão</b>
+                </Checkbox>
 
-                <Form.Item style={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
-                    <Button style={{ backgroundColor: "#610E00", borderRadius: 15, height: 40, width: 150}} type="primary" htmlType="submit">Finalizar Pedido!</Button>
+                <Form.Item style={{ borderColor: ` ${colorInput}`, borderRadius: 8, marginTop: 10 }}>
+                    <Input disabled={componentDisabled} placeholder="Digite o endereço desejado" />
+                </Form.Item>
+
+                <Form.Item required name="pagamento" label={<label style={{ fontSize: 22, color: "#610E00" }}><b>Forma de Pagamento</b></label>}>
+                    <Select style={{ width: "180px", border: "2px solid", borderRadius: 8 }}>
+                        <Select.Option value="Pix">Pix</Select.Option>
+                        <Select.Option value="Cartao de crédito">Cartão de Credito</Select.Option>
+                        <Select.Option value="Dinheiro">Dinheiro</Select.Option>
+                    </Select>
+                </Form.Item>
+
+                <Title level={4} style={{ backgroundColor: "#610E00", width: 260, borderRadius: 15, height: 45, color: "white", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    Valor do Pedido: {(quantidadeTotal * produto.preco)}
+                </Title>
+
+                <Form.Item style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <Button style={{ marginTop: 20, backgroundColor: "#610E00", borderRadius: 15, height: 40, width: 150 }} type="primary" htmlType="submit">Finalizar Pedido!</Button>
                 </Form.Item>
             </Form>
         </div>
     )
-
 }
 
 export default CompraProduto
-
