f262c253363f5a1d31435d5857531dec2f639b97
Kaue-C-Matos <kauecostamatos31@gmail.com>
2024-06-22 18:42:27 -0300

    adicionado aviso caso quantidade disponível do produto acabe

--- front-end/src/components/ProdutosFarmacia.js
+++ front-end/src/components/ProdutosFarmacia.js
@@ -1,4 +1,4 @@
-import { DeleteFilled, EditOutlined } from "@ant-design/icons"
+import { DeleteFilled, EditOutlined, WarningOutlined } from "@ant-design/icons"
 import { Flex, Modal, Typography } from "antd"
 import axios from "axios"
 import { useState } from "react"
@@ -29,16 +29,25 @@ function ProdutosFarmacia({imagem, imgDescricao, nome, preco, quantidade, editar
         }, 2000)
     }
 
+    let display;
+
+    if (quantidade === 0){
+        display= "block"
+    } else {
+        display = "none"
+    }
+
     const card = {padding: "0", background: "#B60000", position: "relative", border: "3px solid", maxWidth: "100%"}
 
     return(
         <Flex style={card}>
             <img src={imagem} alt={imgDescricao} width="100px" style={{backgroundColor: "#fff"}}/>
             <div style={{marginLeft: "10px", textAlign: "left"}}>
-                <Title style={{color: "#fff", fontSize: "18px", whiteSpace: "pre-wrap"}}>{nome}</Title>
+                <Title style={{color: "#fff", fontSize: "18px", width: "95%"}}>{nome}</Title>
                 <Text style={{color: "#fff", fontSize: "16px"}}>R$ {preco}</Text><br/>
                 <Text style={{color: "#fff", fontSize: "16px"}}>unidades: {quantidade}</Text>
             </div>
+            <WarningOutlined style={{position: "absolute", top: "10px", right: "10px", color: "#fff", display: display}}/>
             <div style={{position: "absolute", bottom: "10px", right: "10px", display: "flex", gap: 10}}>
                 <EditOutlined onClick={editar} style={{color: "#fff", fontSize: "18px"}}/>
                 <DeleteFilled onClick={showModal} style={{color: "#fff", fontSize: "18px"}}/>
